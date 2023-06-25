73419c1f064865ff6744484b2d09818bbf1de29a
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-06-25 14:36:35 +0200

    Add basic landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,59 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+	import Icon from '@iconify/svelte';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
+	let showMenu = false;
+
+	function toggleMenu() {
+		showMenu = !showMenu;
+	}
+</script>
+
+<div class="flex flex-col h-screen">
+	<!-- Navigation -->
+	<nav class="flex items-center justify-between p-4 border-b border-b-white">
+		<a href="/" class="flex justify-between items-center gap-2">
+			<Icon icon="ant-design:check-circle-filled" class="text-3xl" />
+			<span class="text-2xl font-bold">NextDo</span>
+		</a>
+		<button class="btn" on:click={toggleMenu}>
+			<Icon icon="ant-design:menu-outlined" class="text-2xl" />
+		</button>
+	</nav>
+
+	<!-- Hidden menu for mobile -->
+	{#if showMenu}
+		<ul
+			class="flex flex-col items-center justify-center gap-8 fixed top-0 left-0 w-full h-full z-10 bg-surface-backdrop-token"
+			transition:slide={{ duration: 200, axis: 'x' }}
+			on:click={toggleMenu}
+			on:keydown={(e) => {
+				if (e.key === 'Enter' || e.key === ' ') {
+					toggleMenu();
+				}
+			}}
+		>
+			<li>
+				<a href="/login" class="btn variant-ghost w-52 text-xl font-bold" on:click|stopPropagation
+					>Login</a
+				>
+			</li>
+			<li>
+				<a
+					href="/register"
+					class="btn variant-filled-primary w-52 text-xl font-bold"
+					on:click|stopPropagation>Register</a
+				>
+			</li>
 		</ul>
-	</div>
+	{/if}
+
+	<!-- Hero -->
+	<section class="flex-1 flex flex-col items-center justify-center gap-8 p-8">
+		<h1 class="text-5xl font-bold text-center">Organize your work and life, finally.</h1>
+		<p class="text-xl text-center">
+			Become focused, organized, and calm with Nextdo. The world’s #1 task manager and to-do list
+			app.
+		</p>
+	</section>
 </div>
