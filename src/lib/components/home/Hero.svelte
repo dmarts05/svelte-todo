8a3736cf2f7a9c0cd940fa2bc02848978427b881
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-06-25 17:32:20 +0200

    feat(Hero): Make Hero section responsive

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -2,17 +2,18 @@
 	import illustration from '$lib/assets/hero-illustration.svg';
 </script>
 
-<main class="flex-1 flex flex-col items-center justify-center gap-8 p-8">
-	<h1 class="text-5xl font-bold text-center">
+<main class="flex flex-col items-center justify-center flex-1 gap-8 p-8 md:p-24 xl:p-40">
+	<h1 class="text-5xl font-bold text-center md:text-6xl xl:text-7xl max-w-prose">
 		Organize your <span class="text-primary-500">work</span> and
 		<span class="text-primary-500">life</span>, finally.
 	</h1>
-	<p class="text-xl text-justify">
-		Become focused, organized, and calm with NextDo. The world's #1 task manager and to-do list app.
+	<p class="text-xl text-center md:text-2xl xl:text-3xl max-w-prose">
+		Become <strong>focused</strong>, <strong>organized</strong>, and <strong>calm</strong> with
+		<strong>NextDo</strong>. The world's <strong>#1 task manager </strong> and to-do list app.
 	</p>
 	<img
 		src={illustration}
 		alt="A illustration of a completed task in a mobile phone"
-		class="self-center w-64 mt-16"
+		class="self-center w-1/2 max-w-xl mt-16"
 	/>
 </main>
