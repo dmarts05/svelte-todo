8915f2b9bdaea2f1bd85a315c574302062df3085
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-06-27 00:08:50 +0200

    feat(auth): Prepare app for auth with supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,26 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => subscription.unsubscribe();
+	});
 </script>
 
 <slot />
